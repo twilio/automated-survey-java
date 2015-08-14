b3c9e6abbd8a5554f0443ec446a8ac20ebf59e78
Brandon Johnson <joh11115@umn.edu>
2015-08-14 08:37:57 -0500

    Implemented basic file IO for question import

--- src/main/java/com/twilio/survey/Config.java
+++ src/main/java/com/twilio/survey/Config.java
@@ -1,20 +1,25 @@
 package com.twilio.survey;
 
+import java.io.File;
 import java.util.Map;
+import java.util.Scanner;
 
 import com.mongodb.MongoClientURI;
+import com.google.gson.*; // TODO: Resolve wildcard import
+import com.twilio.survey.models.Question;
 
 
 public class Config {
   public int port;
   public MongoClientURI mongoURI;
+  public Question[] questions;
 
   public Config() {
     Map<String, String> env = System.getenv();
 
     // Set defaults, and override with environment variables, if present.
     port = 4567; // default to port 4567, the idiomatic Spark port.
-    String envMongoURL = ""; // default to localhost, the idiomatic MongoDB host. The driver will
+    String envMongoURL = "mongodb://localhost:27017"; // default to localhost, the idiomatic MongoDB host. The driver will
                              // use the idiomatic MongoDB port by default.
 
     // Check the environment for the presence of configured variables.
@@ -25,10 +30,31 @@ public class Config {
       envMongoURL = env.get("MONGOLAB_URI");
     } else if (env.containsKey("MONGO_URL")) {
       envMongoURL = env.get("MONGO_URL");
-    } else {
-      envMongoURL = "mongodb://localhost";
-      env.put("MONGO_URL", envMongoURL);
     }
+    
     mongoURI = new MongoClientURI(envMongoURL);
+    questions = parseQuestionFile();
+  }
+
+  private Question[] parseQuestionFile() {
+    String questionFileAsJson = importQuestionFile();
+    Gson gson = new Gson();
+    Question[] q = gson.fromJson(questionFileAsJson, Question[].class);
+    return q;
+  }
+  
+  private String importQuestionFile(){
+    String questionFileAsString = "";
+    try {
+      Scanner questionFile = new Scanner(new File("questions.json"));
+      while (questionFile.hasNextLine()) {
+        questionFileAsString += questionFile.nextLine();
+      }
+      questionFile.close();
+    } 
+    catch (Exception e){
+      System.err.println(e.getMessage());
+    }
+    return questionFileAsString;
   }
 }
---
+++ src/main/java/com/twilio/survey/models/Question.java
@@ -0,0 +1,14 @@
+package com.twilio.survey.models;
+
+public class Question {
+  private String type;
+  private String text;
+  
+  public String getType() {
+    return this.type;
+  }
+  
+  public String getText() {
+    return this.text;
+  }
+}
