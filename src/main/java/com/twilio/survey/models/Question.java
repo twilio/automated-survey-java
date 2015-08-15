8e0a359cbf0844e881f03044eb80e86e6c899245
Brandon Johnson <joh11115@umn.edu>
2015-08-15 10:13:39 -0500

    Enabled unit testing framework

--- src/main/java/com/twilio/survey/models/Question.java
+++ src/main/java/com/twilio/survey/models/Question.java
@@ -3,11 +3,16 @@ package com.twilio.survey.models;
 public class Question {
   private String type;
   private String text;
-  
+
+  public Question(String type, String text) {
+    this.type = type;
+    this.text = text;
+  }
+
   public String getType() {
     return this.type;
   }
-  
+
   public String getText() {
     return this.text;
   }
---
+++ src/test/java/com/twilio/survey/ServerTest.java
@@ -0,0 +1,23 @@
+package com.twilio.survey;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ServerTest {
+  public static Config config;
+  
+  @BeforeClass
+  public static void setUpBeforeClass() throws Exception {
+    
+  }
+
+  @Test
+  public void questionsImportedSuccessfully() {
+    config = new Config();
+    assertTrue(config.questions.length > 0);
+    assertNotNull(config.questions);
+  }
+
+}
